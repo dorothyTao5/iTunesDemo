62ef50f37de8f4db5f7d34d173866fdf4b5c8020
dorothy <dorothy@goonsdesign.com>
2022-05-12 16:50:47 +0800

    delete unused code

--- iTunesDemo/BaseViewController/BaseNavigationViewController.swift
+++ iTunesDemo/BaseViewController/BaseNavigationViewController.swift
@@ -14,34 +14,4 @@ class BaseNavigationViewController: UINavigationController {
         modalPresentationStyle = .overFullScreen
         navigationBar.titleTextAttributes = [.font: UIFont.systemFont(ofSize: 16, weight: .medium)]
     }
-
-    @discardableResult
-    func setTransparentBar() -> Self {
-        navigationBar.isTranslucent = true
-        navigationBar.setBackgroundImage(UIImage(), for: .default)
-        navigationBar.shadowImage = UIImage()
-        return self
-    }
-    
-    @discardableResult
-    func setBarTintColor(color: UIColor) -> Self {
-        navigationBar.barTintColor = color
-        navigationBar.isTranslucent = false
-        navigationBar.shadowImage = UIImage()
-        return self
-    }
-    
-    override var childForStatusBarStyle: UIViewController? {
-        return self.topViewController
-    }
-}
-
-extension UINavigationController {
-    var base: BaseNavigationViewController? {
-        return self as? BaseNavigationViewController
-    }
-    
-    open override var preferredStatusBarStyle: UIStatusBarStyle {
-        return topViewController?.preferredStatusBarStyle ?? .default
-    }
 }
